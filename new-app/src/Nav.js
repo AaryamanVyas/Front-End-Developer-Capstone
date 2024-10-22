e395c1d0f3fa9f42b16e87a2efaec4a7e7e8603d
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-22 04:58:26 +0000

    Nav.js added more options

--- new-app/src/Nav.js
+++ new-app/src/Nav.js
@@ -6,6 +6,9 @@ function Nav() {
                 <li><a href="#home">Home</a></li>
                 <li><a href="#menu">Menu</a></li>
                 <li><a href="#about">About Us</a></li>
+                <li><a href="#Reservations">Reservations </a></li>
+                <li><a href="#order-online">Order Online</a> </li>
+                <li><a href="#login">Login Us</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
