5f1460e534e7e837733f1cb99c20f8bbe591325e
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-22 05:52:31 +0000

    changed some css

--- new-app/src/Header.js
+++ new-app/src/Header.js
@@ -2,6 +2,9 @@ import './App.css';
 function Header() {
     return(
         <header className='header'>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
             <div className="call-to-action">
                 <h1>Reserve a Table</h1>
                 <button>Click Here</button>
