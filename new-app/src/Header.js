d0626eb84193a6e2d1a166b52b317f86c2840b8e
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-22 08:53:50 +0000

    trying to implement flexbox on image overlappig

--- new-app/src/Header.js
+++ new-app/src/Header.js
@@ -1,15 +1,24 @@
+import logo1 from './waitress.avif'
 import './App.css';
 function Header() {
     return(
         <header className='header'>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <div className="call-to-action">
-                <h1>Reserve a Table</h1>
-                <button>Click Here</button>
-            </div>
-
+            <>
+            
+                <div className='text-block'>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>We are a family owned Mediterranean 
+                        restaurant, focused on traditional recipes 
+                        served with a modern twist.</p>
+                    <div className="call-to-action">
+                        <button>Reserve a Table</button>
+                    </div>
+                    <div className='image-block'>
+                        <img src={logo1} alt='Delicous food'/>
+                    </div>
+                </div>
+            </>
         </header>
     )
 }
