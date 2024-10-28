8e2486e6e187cef69ec9614ff886bc62d2f6ff78
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-28 07:31:15 +0000

    commit

--- new-app/src/Main.js
+++ new-app/src/Main.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
+import BookingForm from './BookingForm';
+
 function Main() {
-    return(
-        <main className="main-content">
-            <h1>Welcome to little Lemon</h1>
-            <p>Enjoy fresh, locally sourced Mediterranean dishes!</p>
-        </main>
-    )
+   // State for available times
+   const [availableTimes, setAvailableTimes] = useState([
+      '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
+   ]);
+
+   return (
+      <main className="main-content">
+         <h1>Welcome to Little Lemon</h1>
+         <p>Enjoy fresh, locally sourced Mediterranean dishes!</p>
+         
+         {/* Render BookingForm and pass availableTimes as a prop */}
+         <BookingForm availableTimes={availableTimes} />
+      </main>
+   );
 }
+
 export default Main;
