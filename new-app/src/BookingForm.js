a240fe7063d26d04ebcc06840fb2d6f824b02655
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-28 07:01:06 +0000

    BookingForm.js

---
+++ new-app/src/BookingForm.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+function BookingForm() {
+    const [date,setDate]=useState('');
+    const [time, setTime]=useState('17:00');
+    const [guests, setGuests]=useState(1);
+    const [occasion, setOccasion]=useState('Birthday');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    };
+    return (
+        <form onSubmit={handleSubmit} style={{display:'gird',maxWidth:'200px', gap:'20px'}}>
+            <label htmlFor="res-date">Choose date</label>
+            <input type="data" id="res-data" value={data} onChange={(e) => setDate(e.target.value)}/>
+            <label htmlFor="res-time">Choose time</label>
+            <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </select>
+            <label htmlFor="guests">Number of guests</label>
+            <input type="number" id="guests" placeholder="1" min="1" max="10" value={guests} onChange={(e) => setGuests(e.target.value)} />
+
+            <label htmlFor="occasion">Occasion</label>
+            <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </select>
+
+            <input type="submit" value="Make Your reservation" />
+        </form>
+    )
+}
