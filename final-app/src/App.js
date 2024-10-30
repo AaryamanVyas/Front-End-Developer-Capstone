f35ea5c14762d05315ce40fc4eb56e7ab2db9041
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-30 07:20:22 +0000

    added image

--- final-app/src/App.js
+++ final-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import nav from
 function App() {
   return (
     <>
---
+++ final-app/src/components/Nav.js
@@ -0,0 +1,12 @@
+import logo from '../images/little_lemon.png'
+function Nav() {
+    return (
+        <nav className="nav">
+            <img src={logo} alt='logo'/>
+            <ul>
+
+            </ul>
+        </nav>
+    )
+};
+export default Nav;
