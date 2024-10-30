cb47929b079bd3c6e97bf175625161f22641b10a
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-10-30 07:28:13 +0000

    added nav items

--- final-app/src/components/Nav.js
+++ final-app/src/components/Nav.js
@@ -11,7 +11,13 @@ function Nav() {
                 <div className='bar'></div>
             </div>
             <ul>
-
+                <li><a href="#home">Home</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#Reservations">Reservations </a></li>
+                <li><a href="#order-online">Order Online</a> </li>
+                <li><a href="#login">Login Us</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     )
