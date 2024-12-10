b06599b9336f0355e9036fa3cfd3615015553ad6
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-12-10 17:30:19 +0000

    updated code with time fixed

--- final-app/src/App.js
+++ final-app/src/App.js
@@ -3,13 +3,14 @@ import Nav from './components/Nav'
 import Header from './components/Header';
 import BookingPage from './components/BookingPage';
 import { Route, Routes } from 'react-router-dom';
+import Main from './components/Main';
 function App() {
   return (
     <>
       <Nav />
       <Header />
       <Routes>
-        <Route path='/booking' element={<BookingPage/>} />
+        <Route path='/booking' element={<Main/>} />
       </Routes>
       {/* <BookingForm/> */}
     </>
