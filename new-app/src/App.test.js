ad170438c338b3ca7ecd4a38a4f57c8efbb26e90
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-12-17 07:22:08 +0000

    deleted old file

--- new-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the Little Lemon welcome message', () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to Little Lemon/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
